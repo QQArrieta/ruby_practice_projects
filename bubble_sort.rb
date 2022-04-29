d4b36c13dd21515a3b8ddda655b25925fed10c2e
Qneil Arrieta <arrietaqneil999@gmail.com>
2022-04-29 18:35:10 +0800

    Remove temp[]

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,12 +1,9 @@
 def bubble_sort(array)
-    temp = []
     (array.length-1).times do
         i = 0
         until i==array.length-1 do
             if array[i]>array[i+1]
-                temp[0] = array[i]
-                array[i] = array[i+1]
-                array[i+1] = temp[0]
+                array[i], array[i + 1] = array[i + 1], array[i]
                 i += 1
             else
                 i += 1
